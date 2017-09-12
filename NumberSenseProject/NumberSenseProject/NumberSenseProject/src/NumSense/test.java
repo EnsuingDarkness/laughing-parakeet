807f2bb4ce9cf89a6921eb67f993808ff00efb3e
Trent Phan <phantrent@gmail.com>
2017-09-11 22:02:54 -0500

    Add files via upload

---
+++ NumberSenseProject/NumberSenseProject/NumberSenseProject/src/NumSense/test.java
@@ -0,0 +1,18 @@
+package NumSense;/**
+ * Created by 586905 on 4/4/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
